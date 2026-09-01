26e78028deae8ef0fe1f2dd2d99fdeae55944251
Cchin03 <136949913+Cchin03@users.noreply.github.com>
2026-09-01 14:33:08 +0800

    Update sidebar.tsx

--- components/navbar/sidebar.tsx
+++ components/navbar/sidebar.tsx
@@ -47,6 +47,7 @@ const adminModules = [
       { label: 'Audit Log', path: 'auditLog' },
       { label: 'Assets', path: 'assets' },
       { label: 'Maintenance', path: 'maintainApprove' },
+      { label: 'Deleted Assets', path: 'deleteAssets' },
       { label: 'Bulk Import', path: 'addMoreAssets' }
 
     ],
